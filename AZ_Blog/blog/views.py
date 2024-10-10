09b0a19f368eabcc282fe5889f8ffef089cc8f3e
Ziad Khaled <ziadkh99@hotmail.com>
2024-10-10 17:37:45 +0300

    Create sup-project bolg and Intialize its routes and templates

---
+++ AZ_Blog/blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'ziad khaled'.title(),
+        'title': 'Blog Post 1',
+        'content': 'Firt post content...',
+        'date_posted': 'August 5, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content...',
+        'date_posted': 'April 27, 2023'
+    },
+    {
+        'author': 'Ramy Hafez',
+        'title': 'Blog Post 3',
+        'content': 'Third post content...',
+        'date_posted': 'June 13, 2023'
+    },
+]
+
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': posts,
+        'title': 'AZ-Blog Home',
+    }
+
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'AZ-Blog About'})
+    # return HttpResponse('<h1>About Page</h1>')
+
+
+
