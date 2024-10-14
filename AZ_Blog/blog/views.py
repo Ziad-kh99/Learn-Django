f20079b029686fe6bbf9cec9fbc479b2bbffde35
Ziad Khaled <ziadkh99@hotmail.com>
2024-10-14 22:26:45 +0300

    Connect with migrate to DB and use data from it

--- AZ_Blog/blog/views.py
+++ AZ_Blog/blog/views.py
@@ -1,33 +1,34 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Post
 
 
-posts = [
-    {
-        'author': 'ziad khaled'.title(),
-        'title': 'Blog Post 1',
-        'content': 'Firt post content...',
-        'date_posted': 'August 5, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content...',
-        'date_posted': 'April 27, 2023'
-    },
-    {
-        'author': 'Ramy Hafez',
-        'title': 'Blog Post 3',
-        'content': 'Third post content...',
-        'date_posted': 'June 13, 2023'
-    },
-]
+# posts = [
+#     {
+#         'author': 'ziad khaled'.title(),
+#         'title': 'Blog Post 1',
+#         'content': 'Firt post content...',
+#         'date_posted': 'August 5, 2022'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content...',
+#         'date_posted': 'April 27, 2023'
+#     },
+#     {
+#         'author': 'Ramy Hafez',
+#         'title': 'Blog Post 3',
+#         'content': 'Third post content...',
+#         'date_posted': 'June 13, 2023'
+#     },
+# ]
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'AZ-Blog Home',
     }
 
