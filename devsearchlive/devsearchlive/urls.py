db5d6f012b692998c746723c00df37d441016307
Ziad Khaled <ziadkh99@hotmail.com>
2024-12-26 20:32:58 +0200

    Learn about views, urls, and templates

--- devsearchlive/devsearchlive/urls.py
+++ devsearchlive/devsearchlive/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-
-def projects(request):
-    return HttpResponse('This is our projects page.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects)
+    path('', include('projects.urls'))
 ]
--- devsearchlive/projects/views.py
+++ devsearchlive/projects/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render         # used to reneder templates.
+from django.http import HttpResponse
 
-# Create your views here.
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
+def projects(request):
+    name = 'Ziad Khaled'
+
+    context = {
+        'name': name,
+        'projects': projectsList
+    }
+
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObject = None
+
+    for project in projectsList:
+        if project['id'] == pk:
+            projectObject = project
+
+    context = {
+        'project': projectObject,
+        'pk': pk
+    }
+
+    return render(request, 'projects/single_project.html', context)
+
+
+
