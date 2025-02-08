9e732a56d3eaa5743bad3de83e9f774c239fdf31
Ziad Khaled <ziadkh99@hotmail.com>
2025-02-08 18:34:34 +0200

    setup a new virtual environment

--- AZ_Blog/blog/views.py
+++ AZ_Blog/blog/views.py
@@ -3,28 +3,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-# posts = [
-#     {
-#         'author': 'ziad khaled'.title(),
-#         'title': 'Blog Post 1',
-#         'content': 'Firt post content...',
-#         'date_posted': 'August 5, 2022'
-#     },
-#     {
-#         'author': 'Jane Doe',
-#         'title': 'Blog Post 2',
-#         'content': 'Second post content...',
-#         'date_posted': 'April 27, 2023'
-#     },
-#     {
-#         'author': 'Ramy Hafez',
-#         'title': 'Blog Post 3',
-#         'content': 'Third post content...',
-#         'date_posted': 'June 13, 2023'
-#     },
-# ]
-
-
 # Create your views here.
 def home(request):
     context = {
